b8f4f06c16060408e85fc1c5cc64ce1e68c4808f
Andrew <anor@jwor.org>
2022-02-03 09:19:06 -0500

    negrita y color rojo

--- prueba.py
+++ prueba.py
@@ -1,1 +1,7 @@
-print('hola mundo')
+class veiculo:
+    def __init__(self,color,altura):
+        self.color = color
+        self.altura = altura
+
+veiculo1 = veiculo('Rojo', 15)
+print(f'Su veiculo es: {veiculo1}')
